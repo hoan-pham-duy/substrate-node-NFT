75fe13352b728344f63664667fd63790b3b247d2
Pham Duy Hoan <phamduyhoan95@gmail.com>
2021-12-21 20:12:11 +0700

    Change nft_object_base_64_str: [u8; MAX_FILE_SIZE_IN_BYTE]

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -22,6 +22,7 @@ pub mod pallet {
 	#[cfg(feature = "std")]
 	use frame_support::serde::{Deserialize, Serialize};
 
+	const MAX_FILE_SIZE_IN_BYTE: usize = 100 * 1024; //MAX 100 MB
 	type AccountOf<T> = <T as frame_system::Config>::AccountId;
 	type BalanceOf<T> =
 		<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
@@ -157,7 +158,7 @@ pub mod pallet {
 		///
 		/// The actual kitty creation is done in the `mint()` function.
 		#[pallet::weight(100)]
-		pub fn create_kitty(origin: OriginFor<T>, nft_object_base_64_str: bool) -> DispatchResult {
+		pub fn create_kitty(origin: OriginFor<T>, nft_object_base_64_str: [u8; MAX_FILE_SIZE_IN_BYTE]) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
 
 			let kitty_id = Self::mint(&sender, None, None)?;
